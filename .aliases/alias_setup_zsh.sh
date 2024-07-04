64f38e41f19f3b17c63b06e4a49e625bcf2d464b
Vedant Singhania <vedant.singhania@gmail.com>
2024-07-04 14:23:14 -0700

    zsh setup

---
+++ .aliases/alias_setup_zsh.sh
@@ -0,0 +1,18 @@
+# Define the content to add to ~/.zshrc
+content='for file in $HOME/.aliases/*.sh; do
+    if [ -r "$file" ]; then
+        source "$file"
+    fi
+done'
+
+# Check if ~/.zshrc exists and is writable
+zshrc_file="$HOME/.zshrc"
+if [ -f "$zshrc_file" ] && [ -w "$zshrc_file" ]; then
+    # Add the content to the top of ~/.zshrc
+    temp_file=$(mktemp)
+    echo "$content" | cat - "$zshrc_file" > "$temp_file" && mv "$temp_file" "$zshrc_file"
+    echo "Updated $zshrc_file successfully."
+else
+    echo "Error: $zshrc_file does not exist or is not writable."
+    exit 1
+fi
