e35019bf7977df9b82a7e0e7937a8248c1f6b9d6
Angel Barre <skynetoffc@gmail.com>
2024-07-07 20:29:00 -0500

    Update more aliases for github and documentation for each

--- aliases/git.sh
+++ aliases/git.sh
@@ -1,3 +1,8 @@
 alias gad="git add ."
 alias gcm="git commit -m"
 alias gp="git push"
+alias gpl="git pull"
+alias gs="git status"
+alias gd="git diff"
+alias gb="git branch"
+alias gco="git checkout"
